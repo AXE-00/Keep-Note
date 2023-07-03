8a11ac66c590facd60584e8fd75b3a33e1253eb8
Ashutosh <ashutosh.k.work@gmail.com>
2023-07-04 01:03:11 +0530

    displayed search component; added search input; added custom CSS

--- keep-note/src/app/components/note-view/note-view.component.ts
+++ keep-note/src/app/components/note-view/note-view.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { Note } from 'src/app/models/note.model';
 
 @Component({
-  selector: 'app-note-view',
-  templateUrl: './note-view.component.html',
-  styleUrls: ['./note-view.component.css']
+	selector: 'app-note-view',
+	templateUrl: './note-view.component.html',
+	styleUrls: ['./note-view.component.css'],
 })
 export class NoteViewComponent {
+	notes: Note[] = [];
+	noteData: Note[] = [];
 
+	search(response: string) {
+		if (response === '' || !response) {
+			this.notes = this.noteData;
+		} else {
+			this.notes = this.noteData;
+			this.notes = this.notes?.filter((notes) =>
+				notes.title?.startsWith(response)
+			);
+		}
+	}
+	insertsData(response: Note) {
+		this.notes.push(response);
+	}
 }
--- keep-note/src/app/components/search-note/search-note.component.ts
+++ keep-note/src/app/components/search-note/search-note.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
-  selector: 'app-search-note',
-  templateUrl: './search-note.component.html',
-  styleUrls: ['./search-note.component.css']
+	selector: 'app-search-note',
+	templateUrl: './search-note.component.html',
+	styleUrls: ['./search-note.component.css'],
 })
 export class SearchNoteComponent {
+	searchText: string = '';
 
+	@Output()
+	searchTextChanged: EventEmitter<string> = new EventEmitter<string>();
+
+  onSearchTextChanged(){
+    this.searchTextChanged.emit(this.searchText)
+  }
 }
