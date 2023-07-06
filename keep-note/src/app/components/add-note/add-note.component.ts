a7e516f30eaeec931b61cb141f17a73b05fd127e
Ashutosh <ashutosh.k.work@gmail.com>
2023-07-07 01:36:34 +0530

    created addNote() function that adds a note by emitting data to keep-note-service.ts

--- keep-note/src/app/components/add-note/add-note.component.ts
+++ keep-note/src/app/components/add-note/add-note.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Note } from 'src/app/models/note.model';
+import { KeepNoteService } from 'src/app/services/keep-note.service';
 
 @Component({
-  selector: 'app-add-note',
-  templateUrl: './add-note.component.html',
-  styleUrls: ['./add-note.component.css']
+	selector: 'app-add-note',
+	templateUrl: './add-note.component.html',
+	styleUrls: ['./add-note.component.css'],
 })
 export class AddNoteComponent {
+	note: Note = {};
 
+	@Output()
+	noteAdded: EventEmitter<Note> = new EventEmitter<Note>();
+
+	constructor(private noteAdd: KeepNoteService) {}
+
+	addNote() {
+		this.noteAdd.addNote(this.note).subscribe({
+			next: (data) => {
+				alert('Note added');
+				this.noteAdded.emit(data);
+				this.note = {};
+			},
+			error: (Error) => {
+				alert('Failed to insert data');
+				console.log(Error);
+			},
+		});
+	}
 }
